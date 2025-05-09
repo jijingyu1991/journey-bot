4e2a6938bbb4f65a7e8e4b986a2d7abb7f32b053
guantingting <guantingting@saicmotor.com>
2025-05-09 14:33:18 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/*
+ * @Date: 2025-04-10 13:31:58
+ * @LastEditors: guantingting
+ * @LastEditTime: 2025-05-09 14:32:35
+ */
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
